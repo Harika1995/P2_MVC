0d58dfa392db74887ae2461e86a4e70d8d4b9ef6
Harika Naidu <nagaharika.n@gmail.com>
2019-02-08 17:04:06 -0600

    commit

--- P2_MVC/tests/UnitTests/UnitTest1.cs
+++ P2_MVC/tests/UnitTests/UnitTest1.cs
@@ -5,10 +5,28 @@ namespace UnitTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        int Multiplication(int a,int b)
         {
-
+            return a * b;
         }
+        [Fact]
+        public void Multipilaction_WorksWithTwoPositiveIntegers()
+        {
+            var expected = 4;
+            var a = 2;
+            var b = 2;
+            var actual = Multiplication(a, b);
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(4, 2, 2)]
+        [InlineData(4, -2, -2)]
+        [InlineData(0, 0, 0)]
+        public void Multiplication_WorksWithManyInputs(int exp, int n1, int n2)
+        {
+            Assert.Equal(exp, Multiplication(n1, n2));
+        }
+
+
     }
 }
